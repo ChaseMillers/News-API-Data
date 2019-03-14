23c2a599614d7ea2bef46ba53019547da3373514
Chase Miller <chasemillers@outlook.com>
2019-03-14 13:16:37 -0700

    Features - each news source now displays max number regaless of how many sellected. Javascript now makes muiltiple calls to API. Max number is now a parameter added to fetched url instead being ran through a loop Bugs- None

--- script.js
+++ script.js
@@ -1,47 +1,135 @@
 'use strict';
 const apiKey = '23bde539d93344af9341bf30f0ac89fc'
 
-function fetchInfo(url, key) {
-  console.log (url)
-
-  fetch (url, key)
-  .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-errorMessage').text(`There are no matching search results`);
-      $('.results').empty();
+function fetchInfo(bbcUrl, newYorkTimesUrl, foxUrl, cnnUrl, elseUrl) {
+  console.log (bbcUrl, newYorkTimesUrl, foxUrl, cnnUrl)
+  const key = {headers: new Headers ({"X-Api-Key": apiKey})};
+  
+  if(bbcUrl !== ''){
+    fetch (bbcUrl,key)
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-errorMessage').text(`There are no matching search results`);
+        $('.results').empty();
     });
-}
-
-function UrlConstruction(searchValue, sortOrder, currentSelectedSources) {
-    console.log(`${searchValue} is the search value`);
-    console.log(`${sortOrder} is how the search will be sorted`);
-    let newsSource = '';
-    for ( let i = 0; i < currentSelectedSources.length; i ++ ){
-      if ( $(currentSelectedSources[i]).is(":checked")){
-          newsSource += $(currentSelectedSources[i]).val()+',';
-      }
   }
-    const key = {headers: new Headers ({"X-Api-Key": apiKey})};
-    let url = 'https://newsapi.org/v2/everything?language=en&pageSize=100&q=' +
-    searchValue + 
-    sortOrder + "sources=" +
-    newsSource; 
-    
-    console.log(`${newsSource} is the new array`);
-    fetchInfo(url, key);
+  else if(newYorkTimesUrl !== ''){
+    fetch (newYorkTimesUrl,key)
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-errorMessage').text(`There are no matching search results`);
+        $('.results').empty();
+      });
+    }
+  else if(foxUrl !== ''){
+      fetch (foxUrl,key)
+      .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+          $('#js-errorMessage').text(`There are no matching search results`);
+          $('.results').empty();
+        });
+      }
+    else if(cnnUrl !== ''){
+        fetch (cnnUrl,key)
+        .then(response => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error(response.statusText);
+          })
+          .then(responseJson => displayResults(responseJson))
+          .catch(err => {
+            $('#js-errorMessage').text(`There are no matching search results`);
+            $('.results').empty();
+          });
+        }
+        else{
+          fetch (elseUrl,key)
+          .then(response => {
+              if (response.ok) {
+                return response.json();
+              }
+              throw new Error(response.statusText);
+            })
+            .then(responseJson => displayResults(responseJson))
+            .catch(err => {
+              $('#js-errorMessage').text(`There are no matching search results`);
+              $('.results').empty();
+            });
+          }
 }
 
+function UrlConstruction(searchValue, sortBy, currentSelectedSources, max) {
+    console.log(`${searchValue} is the search value`);
+    
+    let bbcValue = $('#bbcCheck:checked').val();
+    let newYorkTimesValue = $('#newYorkTimesCheck:checked').val();
+    let foxValue = $('#foxCheck:checked').val();
+    let cnnValue = $('#cnnCheck:checked').val();
+      
+  if(bbcValue == 'bbc-news'){
+    let bbcUrl = 'https://newsapi.org/v2/everything?language=en&sources=bbc-news&' +
+    searchValue + 
+    max +
+    sortBy;
+    fetchInfo(bbcUrl);
+  }
+
+  if(newYorkTimesValue == 'the-new-york-times'){
+    let newYorkTimesUrl = 'https://newsapi.org/v2/everything?language=en&sources=the-new-york-times&' +
+    searchValue + 
+    max +
+    sortBy;
+    fetchInfo(newYorkTimesUrl);
+  }
+
+  if(foxValue == 'fox-news'){
+    let foxUrl = 'https://newsapi.org/v2/everything?language=en&sources=fox-news&' +
+    searchValue + 
+    max +
+    sortBy;
+    fetchInfo(foxUrl);
+  }
+
+  if(cnnValue == 'cnn'){
+    let cnnUrl = 'https://newsapi.org/v2/everything?language=en&sources=cnn&' +
+    searchValue + 
+    max +
+    sortBy;
+    fetchInfo(cnnUrl);
+  }
+
+  else if (currentSelectedSources == undefined){
+    let elseUrl = 'https://newsapi.org/v2/everything?language=en&sources=cnn,the-new-york-times,fox-news,bbc-news&' +
+    searchValue + 
+    max +
+    sortBy;
+    fetchInfo(elseUrl);
+  }
+}
 
 function displayResults(responseJson){ 
     console.log(responseJson);
     $('#js-errorMessage').empty();
-    let max = $('#js-maxResults').val();
+
     let topHTML = '';
     let bbcHTML = '';
     let newHTML = '';
@@ -52,7 +140,7 @@ function displayResults(responseJson){
     let foxValue = $('#foxCheck:checked').val();
     let cnnValue = $('#cnnCheck:checked').val();
  
-    for (let i = 0; i < responseJson.articles.length && i<max; i ++){
+    for (let i = 0; i < responseJson.articles.length; i ++){
       if ( responseJson.articles[i].source.name == "BBC News" && bbcValue == 'bbc-news'){
     bbcHTML +=
     `<div class="newsColumn">
@@ -95,20 +183,20 @@ function displayResults(responseJson){
       $('.foxTitle, #fox').css('display','flex'); 
       }
 
-      else if ( responseJson.articles[i].source.name == "CNN" && cnnValue == 'cnn'){
-        cnnHTML +=
-        `<div class="newsColumn">
-        <h4>${responseJson.articles[i].title}</h4>
-        <img src='${responseJson.articles[i].urlToImage}'>
-        <p>${responseJson.articles[i].description}</p>
-        <a href ="${responseJson.articles[i].url}" target="_blank">
-        ${responseJson.articles[i].url}</a>
-        </div>`;
-        $('#cnn').html(
-          `${cnnHTML}`);
-        $('.cnnTitle, #cnn').css('display','flex'); 
-        }
-        
+    else if ( responseJson.articles[i].source.name == "CNN" && cnnValue == 'cnn'){
+      cnnHTML +=
+      `<div class="newsColumn">
+      <h4>${responseJson.articles[i].title}</h4>
+      <img src='${responseJson.articles[i].urlToImage}'>
+      <p>${responseJson.articles[i].description}</p>
+      <a href ="${responseJson.articles[i].url}" target="_blank">
+      ${responseJson.articles[i].url}</a>
+      </div>`;
+      $('#cnn').html(
+        `${cnnHTML}`);
+      $('.cnnTitle, #cnn').css('display','flex'); 
+      }
+      
     else {
       topHTML +=
       `<div class="newsColumn">
@@ -124,17 +212,18 @@ function displayResults(responseJson){
     }
 } }
 
-
 function watchForm() {
   $('form').submit(event => {
-    let searchValue = $('#js-searchEntry').val()+'&';
-    let sortOrder = 'sortBy='+ $('#js-sortOrder').val()+'&';
-    let currentSelectedSources = $('.js-sourceSelection')
+    let searchValue = 'q=' + $('#js-searchEntry').val()+'&';
+    let max = 'pageSize=' + $('#js-maxResults').val()+'&';
+    let sortBy = 'sortBy='+ $('#js-sortOrder').val();
     let title = $("#js-sortOrder option:selected").text();
+    let currentSelectedSources = $('.js-sourceSelection:checked').val();
+    console.log(`this is ${currentSelectedSources}`);
     $(".topHeadLinesTitle").html(`<h3>${title} Headlines</h3>`)
     event.preventDefault();
     clear();
-    UrlConstruction(searchValue, sortOrder, currentSelectedSources);
+    UrlConstruction(searchValue, sortBy, currentSelectedSources, max);
   });
 }
 
@@ -147,7 +236,6 @@ function clear(){
   $('.results').empty();
 
 }
-
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
